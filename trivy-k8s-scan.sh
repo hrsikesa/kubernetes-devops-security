3651ca7a64eb77a76ebee810c663a72c36c138eb
hrsikesa <45552974+hrsikesa@users.noreply.github.com>
2022-05-15 09:15:35 +0530

    Update trivy-k8s-scan.sh

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -4,7 +4,8 @@
 echo $imageName #getting Image name from env variable
 
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+# We are just skiping this. If you also want to check if image has CRITICAL vulnerability & fail build if it exist
+#docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
     # Trivy scan result processing
     exit_code=$?
